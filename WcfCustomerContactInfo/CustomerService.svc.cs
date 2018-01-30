fc8d4502059a6f9b189c098eeb30168925f0ba7f
Raymond Sanchez <agileraymond@gmail.com>
2018-01-30 09:17:48 -0600

    add customer datacontract

--- WcfCustomerContactInfo/CustomerService.svc.cs
+++ WcfCustomerContactInfo/CustomerService.svc.cs
@@ -1,32 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.ServiceModel;
-using System.ServiceModel.Web;
-using System.Text;
+﻿using WcfCustomerContactInfo.Contracts;
 
 namespace WcfCustomerContactInfo
 {
-    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class CustomerService : ICustomerService
     {
-        public string GetData(int value)
+        public bool AddCustomer(Customer newCustomer)
         {
-            return string.Format("You entered: {0}", value);
-        }
-
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
-        {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
+            // TODO: validate and then call data layer so we can save customer to database
+            return true;
         }
     }
 }
